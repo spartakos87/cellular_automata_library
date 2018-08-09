5a7bab9ec54836d077ddd7d75ae363c82017f948
spartakos87 <serepasf@yahoo.gr>
2018-08-09 20:10:21 +0300

    fix bug

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 
 setup(name='cecullar_automata',
       version='0.1',
+      packages=['cecullar_automata'],
       description='Python library which create cecullar automata',
        author='Serepas Filippas aka spartakos87',
        author_email='serepasf@gmail.com',
