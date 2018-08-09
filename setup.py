4fe6a16173649ae92ce480b68e228bf58333db7c
spartakos87 <serepasf@yahoo.gr>
2018-08-09 18:54:44 +0300

    manipulate and add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cecullar_automata',
+      version='0.1',
+      description='Python library which create cecullar automata',
+       author='Serepas Filippas aka spartakos87',
+       author_email='serepasf@gmail.com',
+      )
