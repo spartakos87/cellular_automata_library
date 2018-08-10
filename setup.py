acea19b273936aeca9cab16deee92d90c69b1627
spartakos87 <serepasf@yahoo.gr>
2018-08-10 17:51:20 +0300

    update setup.py with package which need to be install

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
 
 setup(name='cellular_automata',
       version='0.1',
       packages=['cellular_automata'],
+      python_requires='>3.5',
+      install_requires=['numpy','matplotlib'],
       description='Python library which create cecullar automata',
        author='Serepas Filippas aka spartakos87',
        author_email='serepasf@gmail.com',
